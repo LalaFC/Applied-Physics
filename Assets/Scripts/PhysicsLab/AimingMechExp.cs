80aad41c78b2e6aba01d56e74b60577f1b09ff68
Angelik Operio <122664874+LalaFC@users.noreply.github.com>
2024-09-28 06:00:55 +0800

    Va=ectors Act

--- Assets/Scripts/PhysicsLab/AimingMechExp.cs
+++ Assets/Scripts/PhysicsLab/AimingMechExp.cs
@@ -1,29 +1,103 @@
+using Cinemachine.Utility;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class AimingMechExp : MonoBehaviour
 {
     [SerializeField] GameObject target;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float range, closeRange;
+    [SerializeField] float force;
+    private Rigidbody rb;
+    private bool canShoot = false, isAiming = false, inRange = false, reload = false, isCharging = true;
+    private Vector3 crossProduct, dotProduct, initPos;
+    private float chargeTime;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        crossProduct = Vector3.Cross(Vector3.up, (target.transform.position - transform.position));
+        initPos = transform.position;
     }
 
     // Update is called once per frame
-    private Vector3 crossProduct;
-    private bool canRotate = false;
+
     void Update()
     {
-        
-        if (Input.GetKey(KeyCode.A))
+        if (isCharging)
         {
-            crossProduct = Vector3.Cross(Vector3.forward, (target.transform.position - transform.position));
-            Debug.Log(crossProduct);
-            float angle = Vector3.Angle(Vector3.forward, (target.transform.position - transform.position));
-            transform.Rotate(crossProduct, angle * Time.deltaTime, Space.World);
+            chargeTime += Time.deltaTime;
+            transform.localScale = reload ? Vector3.zero : Vector3.Lerp(Vector3.zero, Vector3.one, chargeTime / 2);
         }
+
+        Vector3 direction = (target.transform.position - initPos).normalized;
+        Vector3 startPt = new Vector3(initPos.x, (initPos.y - (initPos.y - target.transform.position.y)), initPos.z);
+        Vector3 modifiedDir = new Vector3(direction.x, 0, direction.z);
+        RaycastHit hit;
+        if (Physics.Raycast(startPt, modifiedDir, out hit, closeRange))
+        {
+            Debug.Log("Ray hit: " + hit.collider.name);
+        }
+        Debug.DrawRay(startPt, modifiedDir*closeRange, Color.red);
+
+        /*        if (Input.GetKey(KeyCode.A))
+                {
+                    crossProduct = Vector3.Cross(Vector3.forward, (target.transform.position - transform.position));
+                    Debug.Log(crossProduct);
+                    float angle = Vector3.Angle(Vector3.forward, (target.transform.position - transform.position));
+                    transform.Rotate(crossProduct, angle * Time.deltaTime, Space.World);
+                }*/
+
+        inRange = (target.transform.position - transform.position).magnitude <= range ? true : false;
+        isAiming = (inRange && crossProduct.magnitude > 0.1f) ? true : false;
+        canShoot = (inRange && !isAiming && !reload) ? true : false;
+
+        if (inRange && isAiming)
+        {
+            crossProduct = Vector3.Cross(transform.up, (target.transform.position - transform.position));
+            float angle = Vector3.Angle(transform.up, (target.transform.position - transform.position));
+            transform.Rotate(crossProduct, angle * Time.deltaTime, Space.World);
+
+        }
+        Debug.Log(Vector3.Dot(Vector3.down, -transform.up));
+    }
+    private void FixedUpdate()
+    {
+        if(inRange && canShoot)
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            rb.isKinematic = false;
+            rb.velocity = transform.up * force;
+            canShoot = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy Hit!");
+            StartCoroutine(Reload());
+        }
+    }
+    IEnumerator Reload()
+    {
+        reload = true;
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        yield return new WaitForSeconds(2f);
+        transform.localScale = Vector3.zero;
+        transform.position = initPos;
+        transform.Rotate(Vector3.zero);
+        transform.GetChild(0).gameObject.SetActive(true);
+        reload = false;
+    }
+    void OnDrawGizmos()
+    {
+        Vector3 direction = (target.transform.position - initPos).normalized;
+        Vector3 startPt = new Vector3(initPos.x, (initPos.y - (initPos.y - target.transform.position.y)), initPos.z);
+        Vector3 modifiedDir = new Vector3(direction.x, 0, direction.z);
+        Gizmos.DrawRay(startPt, modifiedDir * closeRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position,direction*10);
     }
 }
---
+++ Assets/Scripts/PhysicsLab/PlaneSelectOnMouseHover.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlaneSelectOnMouseHover : MonoBehaviour
+{
+    private Transform previous;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(camRay,out hit, Mathf.Infinity, LayerMask.GetMask("Floor")))
+        {
+            Material hitMat = hit.transform.GetComponent<Renderer>().material;
+            if (hit.transform != previous && hitMat != null)
+            {
+                if (previous != null) previous.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+                hitMat.EnableKeyword("_EMISSION");
+                hitMat.SetColor("_EmissionColor", Color.gray * 0.5f);
+                previous = hit.transform;
+            }
+        }
+    }
+}
