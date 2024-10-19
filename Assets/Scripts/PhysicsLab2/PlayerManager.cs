e737db54f11d09327a8277d07a8c40a8ff2cc978
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-19 15:47:45 +0800

    physics lab bounciness exp

--- Assets/Scripts/PhysicsLab2/PlayerManager.cs
+++ Assets/Scripts/PhysicsLab2/PlayerManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [Header("Components")]
     protected GameObject player;
+    protected Rigidbody playerRb;
+
+    [Header("Motion")]
+    [SerializeField] protected bool isJumping = false;
+    protected float movementSpeed { get; set; }
 
     // Start is called before the first frame update
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/PhysicsLab2/PlayerMovement.cs
+++ Assets/Scripts/PhysicsLab2/PlayerMovement.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : PlayerManager
 {
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float speedMultiplier, initialSpeed = 5, jumpForce;
+    private bool onAir = false;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.A)) movementSpeed = 2;
-        else if (Input.GetKey(KeyCode.LeftControl)) movementSpeed = 0.5f;
-        else movementSpeed = 1;
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.A)) speedMultiplier = 2;
+        else if (Input.GetKey(KeyCode.LeftControl)) speedMultiplier = 0.5f;
+        else speedMultiplier = 1;
+
+        movementSpeed = initialSpeed * speedMultiplier;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+        }
+
+        if (isJumping)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = false;
+            Debug.Log("isJumping...");
+        }
     }
     private void FixedUpdate()
     {
-        player.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed);
+        if (!onAir)
+        {
+            playerRb.velocity = (new Vector3(Input.GetAxis("Horizontal") * movementSpeed, playerRb.velocity.y, Input.GetAxis("Vertical") * movementSpeed));
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            onAir = false;
+        }
+        else onAir = true;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+
     }
 }
